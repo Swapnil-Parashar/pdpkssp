751c2b4e6e502a4f38af3ade20edba6e6dba61aa
Swapnil-Parashar <swapnilparashar1001@gmail.com>
2024-03-13 18:30:25 +0530

    1.21

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routess
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
