0c06125102189744f7cac2c3cfafc2d9c3c79f4d
drema <getdrema@gmail.com>
2024-03-18 22:37:09 +0530

    pks1803

--- resources/js/components/layouts/backend/BackendNavbarComponent.vue
+++ resources/js/components/layouts/backend/BackendNavbarComponent.vue
@@ -9,21 +9,19 @@
                 class="sub-header flex items-center gap-4 transition xh:justify-between xh:fixed xh:left-0 xh:w-full xh:p-4 xh:border-y xh:border-[#EFF0F6] xh:bg-white">
                 <div class="flex items-center justify-between md:justify-center gap-4">
                     <div v-if="setting.site_language_switch === enums.activityEnum.ENABLE" class="dropdown-group relative">
-                        <button class="dropdown-btn flex items-center gap-2 h-9 px-3 rounded-lg bg-[#FFEDF4]">
-                            <img :src="language.image" alt="flag" class="w-4 h-4 rounded-full">
-                            <span class="hidden md:block whitespace-nowrap text-xs font-medium capitalize text-heading">
-                                {{ language.name }}
-                            </span>
-                        </button>
-                        <ul v-if="languages.length > 0"
-                            class="p-2 min-w-[180px] rounded-lg shadow-xl absolute top-14 z-10 border border-gray-200 bg-white hidden dropdown-list">
-                            <li @click="changeLanguage(language.id, language.code)" v-for="language in languages"
-                                class="flex items-center gap-2 py-1.5 px-2.5 rounded-md cursor-pointer hover:bg-gray-100">
-                                <img :src="language.image" alt="flag" class="w-4 h-4 rounded-full">
-                                <span class="text-heading capitalize text-sm">{{ language.name }}</span>
-                            </li>
-                        </ul>
-                    </div>
+    <button @mouseover="showDropdown = true" @mouseleave="showDropdown = false" class="dropdown-btn flex items-center gap-2 h-9 px-3 rounded-lg bg-[#FFEDF4]">
+        <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+            <path fill-rule="evenodd" d="M17.293 10.293a1 1 0 0 0-1.414 0L11 14.586V7a1 1 0 1 0-2 0v7.586l-4.293-4.293a1 1 0 1 0-1.414 1.414l6 6a1 1 0 0 0 1.414 0l6-6a1 1 0 0 0 0-1.414z" clip-rule="evenodd" />
+        </svg>
+        <span class="hidden md:block whitespace-nowrap text-xs font-medium capitalize text-heading">
+            Call 01268 744 479
+        </span>
+    </button>
+    <div v-if="showDropdown" class="p-2 min-w-[180px] rounded-lg shadow-xl absolute top-14 z-10 border border-gray-200 bg-white hidden dropdown-list">
+        <p class="py-2 px-4 text-sm text-gray-700">Our experts are ready to assist you with any demand. Feel free to call.</p>
+    </div>
+</div>
+
 
                     <router-link v-if="pos.permission"
                         class="w-9 h-9 rounded-lg flex items-center justify-center text-primary bg-[#FFEBD8]"
@@ -110,7 +108,10 @@ export default {
             pos: {
                 permission: false,
                 url: '',
-            }
+            },
+          
+        showDropdown: false
+    
         }
     },
     computed: {
