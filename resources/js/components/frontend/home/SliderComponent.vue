e0e2c96e957a086df056e180ca724b9020d002b0
Swapnil-Parashar <swapnilparashar1001@gmail.com>
2024-03-23 18:43:12 +0530

    SP1903

--- resources/js/components/frontend/home/SliderComponent.vue
+++ resources/js/components/frontend/home/SliderComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <LoadingComponent :props="loading" />
     <section class="mb-5 sm:mb-3">
-        <div class="pt-12 bg-secondary mobile:hidden">
+        <div class="bg-secondary mobile:hidden tablet:hidden">
             <nav class="w-full flex items-center justify-center">
                 <router-link
                     v-for="(category, index) in categories"
@@ -13,7 +13,7 @@
                     @mouseover.prevent="activeTab = 'category_' + category.slug"
                     class="capitalize text-sm font-semibold tracking-wide px-5 py-4 transition-all duration-300 relative before:content-[''] before:absolute before:bottom-0 before:left-0 before:h-0.5 before:bg-primary hover:text-primary"
                     :class="{
-                        'text-primary before:w-full before:transition-all before:duration-300':
+                        'text-white before:w-full before:transition-all before:duration-300':
                             activeTab === 'category_' + category.slug,
                     }"
                 >
