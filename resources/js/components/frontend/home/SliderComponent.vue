9235db79a20bc081c0aada7fdf936c516135eabf
Swapnil-Parashar <swapnilparashar1001@gmail.com>
2024-03-23 21:57:39 +0530

    SP1903

--- resources/js/components/frontend/home/SliderComponent.vue
+++ resources/js/components/frontend/home/SliderComponent.vue
@@ -3,7 +3,7 @@
     <section class="mb-5 sm:mb-3">
         <div class="bg-secondary mobile:hidden tablet:hidden">
             <!-- <nav class="w-full flex items-center justify-center"> -->
-            <nav class="flex items-center justify-between gap-5">
+            <nav class="container flex items-center justify-between gap-5">
                 <router-link
                     v-for="(category, index) in categories"
                     :key="index"
@@ -12,7 +12,7 @@
                         query: { category: category.slug },
                     }"
                     @mouseover.prevent="activeTab = 'category_' + category.slug"
-                    class="text-white capitalize text-sm font-semibold tracking-wide px-5 py-4 transition-all duration-300 relative before:content-[''] before:absolute before:bottom-0 before:left-0 before:h-0.5 before:bg-primary"
+                    class="text-white capitalize text-sm font-semibold tracking-wide py-4 transition-all duration-300 relative before:content-[''] before:absolute before:bottom-0 before:left-0 before:h-0.5 before:bg-primary"
                     :class="{
                         'text-white before:w-full before:transition-all before:duration-300':
                             activeTab === 'category_' + category.slug,
