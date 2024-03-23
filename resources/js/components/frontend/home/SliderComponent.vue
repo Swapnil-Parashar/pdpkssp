360f0015a98b65f097e0a5a7e3641cbe756d754d
Swapnil-Parashar <swapnilparashar1001@gmail.com>
2024-03-23 18:38:31 +0530

    SP1903

--- resources/js/components/frontend/home/SliderComponent.vue
+++ resources/js/components/frontend/home/SliderComponent.vue
@@ -1,18 +1,108 @@
 <template>
     <LoadingComponent :props="loading" />
     <section class="mb-5 sm:mb-3">
+        <div class="pt-12 bg-secondary mobile:hidden">
+            <nav class="w-full flex items-center justify-center">
+                <router-link
+                    v-for="(category, index) in categories"
+                    :key="index"
+                    :to="{
+                        name: 'frontend.product',
+                        query: { category: category.slug },
+                    }"
+                    @mouseover.prevent="activeTab = 'category_' + category.slug"
+                    class="capitalize text-sm font-semibold tracking-wide px-5 py-4 transition-all duration-300 relative before:content-[''] before:absolute before:bottom-0 before:left-0 before:h-0.5 before:bg-primary hover:text-primary"
+                    :class="{
+                        'text-primary before:w-full before:transition-all before:duration-300':
+                            activeTab === 'category_' + category.slug,
+                    }"
+                >
+                    {{ category.name }}
+                </router-link>
+            </nav>
+            <div v-for="category in categories">
+                <div
+                    v-if="category.children.length > 0"
+                    :class="{
+                        block: activeTab === 'category_' + category.slug,
+                        hidden: activeTab !== 'category_' + category.slug,
+                    }"
+                    class="flex items-start gap-5 pb-5 border-t border-gray-200"
+                >
+                    <div class="w-60 h-80 flex-shrink-0 pt-5 pl-5">
+                        <img
+                            class="w-full h-full object-top object-cover rounded-lg"
+                            :src="category.cover"
+                            alt="category"
+                            loading="lazy"
+                        />
+                    </div>
+                    <div class="w-full h-80 thin-scrolling pt-5 pr-5">
+                        <div class="w-full grid gap-5 grid-cols-3">
+                            <div
+                                v-for="children in category.children"
+                                class="self-start"
+                            >
+                                <h3
+                                    class="text-sm font-semibold capitalize pb-3 border-b border-slate-200"
+                                >
+                                    <router-link
+                                        :to="{
+                                            name: 'frontend.product',
+                                            query: {
+                                                category: children.slug,
+                                            },
+                                        }"
+                                        class="hover:text-primary transition-all duration-300"
+                                    >
+                                        {{ children.name }}
+                                    </router-link>
+                                </h3>
+
+                                <nav
+                                    v-if="children.children.length > 0"
+                                    class="flex flex-col mt-2"
+                                >
+                                    <MenuChildrenComponent
+                                        :categories="children.children"
+                                    />
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="coustom-container">
-            <Swiper v-if="sliders.length > 0" :slides-per-view="1" :speed="1000" :loop="true" :navigation="true"
-                    :pagination="{ clickable: true }" :autoplay="{ delay: 2500 }" :modules="modules"
-                    class="banner-swiper">
+            <Swiper
+                v-if="sliders.length > 0"
+                :slides-per-view="1"
+                :speed="1000"
+                :loop="true"
+                :navigation="true"
+                :pagination="{ clickable: true }"
+                :autoplay="{ delay: 2500 }"
+                :modules="modules"
+                class="banner-swiper"
+            >
                 <SwiperSlide v-for="slider in sliders">
                     <div v-if="slider.link">
                         <a :href="slider.link">
-                            <img class="w-full" :src="slider.image" alt="banner0" loading="lazy">
+                            <img
+                                class="w-full"
+                                :src="slider.image"
+                                alt="banner0"
+                                loading="lazy"
+                            />
                         </a>
                     </div>
                     <div v-else>
-                        <img class="w-full" :src="slider.image" alt="banner" loading="lazy">
+                        <img
+                            class="w-full"
+                            :src="slider.image"
+                            alt="banner"
+                            loading="lazy"
+                        />
                     </div>
                 </SwiperSlide>
             </Swiper>
@@ -21,9 +111,9 @@
 </template>
 
 <script>
-import 'swiper/css';
-import {Navigation, Pagination, Autoplay} from 'swiper/modules';
-import {Swiper, SwiperSlide} from 'swiper/vue';
+import "swiper/css";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/vue";
 import statusEnum from "../../../enums/modules/statusEnum";
 import LoadingComponent from "../components/LoadingComponent";
 
@@ -32,40 +122,56 @@ export default {
     components: {
         Swiper,
         SwiperSlide,
-        LoadingComponent
+        LoadingComponent,
     },
     setup() {
         return {
             modules: [Navigation, Pagination, Autoplay],
-        }
+        };
     },
     data() {
         return {
             loading: {
-                isActive: false
+                isActive: false,
             },
             sliderProps: {
                 search: {
                     paginate: 0,
-                    order_column: 'id',
-                    order_type: 'desc',
-                    status: statusEnum.ACTIVE
-                }
-            }
-        }
+                    order_column: "id",
+                    order_type: "desc",
+                    status: statusEnum.ACTIVE,
+                },
+            },
+        };
     },
     computed: {
         sliders: function () {
-            return this.$store.getters['frontendSlider/lists'];
-        }
+            return this.$store.getters["frontendSlider/lists"];
+        },
+        categories: function () {
+            return this.$store.getters["frontendProductCategory/trees"];
+        },
     },
     mounted() {
         this.loading.isActive = true;
-        this.$store.dispatch("frontendSlider/lists", this.sliderProps.search).then((res) => {
-            this.loading.isActive = false;
-        }).catch((err) => {
-            this.loading.isActive = false;
-        });
-    }
-}
+        this.$store
+            .dispatch("frontendSlider/lists", this.sliderProps.search)
+            .then((res) => {
+                this.loading.isActive = false;
+            })
+            .catch((err) => {
+                this.loading.isActive = false;
+            });
+
+        this.loading.isActive = true;
+        this.$store
+            .dispatch("frontendProductCategory/trees")
+            .then((res) => {
+                this.loading.isActive = false;
+            })
+            .catch((err) => {
+                this.loading.isActive = false;
+            });
+    },
+};
 </script>
