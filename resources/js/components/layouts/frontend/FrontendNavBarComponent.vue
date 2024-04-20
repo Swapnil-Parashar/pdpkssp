7a6b62ec48cde0c04e29d077b57f6d95a2a572b2
Swapnil-Parashar <swapnilparashar1001@gmail.com>
2024-04-21 00:55:24 +0530

    SP1903

--- resources/js/components/layouts/frontend/FrontendNavBarComponent.vue
+++ resources/js/components/layouts/frontend/FrontendNavBarComponent.vue
@@ -6,7 +6,7 @@
                 : 'sm:mb-0 shadow-xs bg-white'
         "
     >
-        <div class="container py-3.5 lg:py-0">
+        <div class="container py-3.5 px-2 lg:py-0">
             <div class="flex items-center justify-between gap-5">
                 <!--  Logo & Mobile Responsive Start -->
                 <div class="flex items-center flex-shrink-0 gap-5">
