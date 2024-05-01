b2ff8e099743d9920d1303d68853d92f5e6772db
Swapnil-Parashar <swapnilparashar1001@gmail.com>
2024-05-01 19:15:39 +0530

    SP1903

--- resources/js/components/frontend/home/CategoryComponent.vue
+++ resources/js/components/frontend/home/CategoryComponent.vue
@@ -1,4 +1,4 @@
-<!-- <template>
+<template>
     <LoadingComponent :props="loading" />
     <section v-if="categories.length > 0" class="sm:mb-5">
         <div class="container min-md:mt-10">
@@ -29,7 +29,7 @@
                             loading="lazy"
                         />
                         <span
-                            class="text-sm sm:text-xl font-medium capitalize text-center py-2 px-3 overflow-hidden block group-hover:text-primary flex-grow"
+                            class="text-sm font-medium capitalize text-center py-2 px-3 overflow-hidden block group-hover:text-primary flex-grow"
                         >
                             {{ category.name }}
                         </span>
@@ -104,127 +104,4 @@ export default {
             });
     },
 };
-</script> -->
-
-
-<template>
-    <LoadingComponent :props="loading" />
-    <section v-if="categories.length > 0" class="sm:mb-5">
-        <div class="container min-md:mt-10">
-            <Swiper
-                dir="ltr"
-                :speed="1000"
-                :loop="true"
-                :navigation="true"
-                :modules="modules"
-                class="navigate-swiper"
-                :breakpoints="breakpoints"
-            >
-                <SwiperSlide
-                    v-for="category in categories"
-                    class="swiper-slide"
-                >
-                    <router-link
-                        :to="{
-                            name: 'frontend.product',
-                            query: { category: category.slug },
-                        }"
-                        class="category-card"
-                    >
-                        <img
-                            class="w-full block"
-                            :src="category.thumb"
-                            alt="category"
-                            loading="lazy"
-                        />
-                        <span class="category-name">{{ category.name }}</span>
-                    </router-link>
-                </SwiperSlide>
-            </Swiper>
-        </div>
-        <FlashSaleComponent />
-    </section>
-</template>
-
-<script>
-import { Navigation, Pagination, Autoplay } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/vue";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import statusEnum from "../../../enums/modules/statusEnum";
-import LoadingComponent from "../components/LoadingComponent";
-import FlashSaleComponent from "./FlashSaleComponent.vue";
-
-export default {
-    name: "CategoryComponent",
-    components: {
-        Swiper,
-        SwiperSlide,
-        LoadingComponent,
-    },
-    setup() {
-        return {
-            modules: [Navigation, Pagination, Autoplay],
-        };
-    },
-    data() {
-        return {
-            loading: {
-                isActive: false,
-            },
-            settings: {
-                itemsToShow: 6,
-                wrapAround: false,
-                snapAlign: "start",
-            },
-            breakpoints: {
-                0: { slidesPerView: "auto", spaceBetween: 16 },
-                640: { slidesPerView: 4, spaceBetween: 24 },
-                768: { slidesPerView: 5, spaceBetween: 24 },
-                1024: { slidesPerView: 6, spaceBetween: 24 },
-            },
-        };
-    },
-    computed: {
-        categories: function () {
-            return this.$store.getters["frontendProductCategory/lists"];
-        },
-    },
-    mounted() {
-        this.loading.isActive = true;
-        this.$store
-            .dispatch("frontendProductCategory/lists", {
-                paginate: 0,
-                order_column: "id",
-                order_type: "asc",
-                parent_id: null,
-                status: statusEnum.ACTIVE,
-            })
-            .then((res) => {
-                this.loading.isActive = false;
-            })
-            .catch((err) => {
-                this.loading.isActive = false;
-            });
-    },
-};
 </script>
-
-<style scoped>
-.swiper-slide {
-    display: flex;
-}
-
-.category-card {
-    display: flex;
-    flex-direction: column;
-    flex: 1;
-}
-
-.category-name {
-    flex-grow: 1;
-    text-align: center;
-    font-size: 1rem; /* adjust font size as needed */
-}
-</style>
