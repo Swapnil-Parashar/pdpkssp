a13f25ce57561042dca5cfd9d1caac3ee2fc2b20
Swapnil-Parashar <swapnilparashar1001@gmail.com>
2024-05-01 22:58:59 +0530

    SP1903

--- resources/js/components/frontend/home/CategoryComponent.vue
+++ resources/js/components/frontend/home/CategoryComponent.vue
@@ -2,6 +2,119 @@
     <LoadingComponent :props="loading" />
     <section v-if="categories.length > 0" class="sm:mb-5">
         <div class="container min-md:mt-10">
+            <!-- <h2 class="text-2xl sm:text-4xl font-bold -mb-10" style="margin-bottom: -3.5rem;">
+                {{ $t("label.browse_by_categories") }}
+            </h2> -->
+            <Swiper
+                dir="ltr"
+                :speed="1000"
+                :loop="true"
+                :navigation="true"
+                :modules="modules"
+                class="navigate-swiper"
+                :breakpoints="breakpoints"
+            >
+                <SwiperSlide
+                    v-for="category in categories"
+                    class="mobile:!w-24"
+                >
+                    <router-link
+                        :to="{
+                            name: 'frontend.product',
+                            query: { category: category.slug },
+                        }"
+                        class="w-full shadow-xs group flex flex-col justify-between"
+                    >
+                        <img
+                            class="w-full block"
+                            :src="category.thumb"
+                            alt="category"
+                            loading="lazy"
+                        />
+                        <span
+                            class="text-sm sm:text-xl font-medium capitalize text-center py-2 px-3 overflow-hidden block group-hover:text-primary flex-grow"
+                        >
+                            {{ category.name }}
+                        </span>
+                    </router-link>
+                </SwiperSlide>
+            </Swiper>
+        </div>
+        <FlashSaleComponent />
+    </section>
+</template>
+
+<script>
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import statusEnum from "../../../enums/modules/statusEnum";
+import LoadingComponent from "../components/LoadingComponent";
+import FlashSaleComponent from "./FlashSaleComponent.vue";
+
+export default {
+    name: "CategoryComponent",
+    components: {
+        Swiper,
+        SwiperSlide,
+        LoadingComponent,
+    },
+    setup() {
+        return {
+            modules: [Navigation, Pagination, Autoplay],
+        };
+    },
+    data() {
+        return {
+            loading: {
+                isActive: false,
+            },
+            settings: {
+                itemsToShow: 6,
+                wrapAround: false,
+                snapAlign: "start",
+            },
+            breakpoints: {
+                0: { slidesPerView: "auto", spaceBetween: 16 },
+                640: { slidesPerView: 4, spaceBetween: 24 },
+                768: { slidesPerView: 5, spaceBetween: 24 },
+                1024: { slidesPerView: 6, spaceBetween: 24 },
+            },
+        };
+    },
+    computed: {
+        categories: function () {
+            return this.$store.getters["frontendProductCategory/lists"];
+        },
+    },
+    mounted() {
+        this.loading.isActive = true;
+        this.$store
+            .dispatch("frontendProductCategory/lists", {
+                paginate: 0,
+                order_column: "id",
+                order_type: "asc",
+                parent_id: null,
+                status: statusEnum.ACTIVE,
+            })
+            .then((res) => {
+                this.loading.isActive = false;
+            })
+            .catch((err) => {
+                this.loading.isActive = false;
+            });
+    },
+};
+</script>
+
+
+<!-- 
+<template>
+    <LoadingComponent :props="loading" />
+    <section v-if="categories.length > 0" class="sm:mb-5">
+        <div class="container min-md:mt-10">
             <Swiper
                 dir="ltr"
                 :speed="1000"
@@ -109,4 +222,4 @@ export default {
             });
     },
 };
-</script>
+</script> -->
