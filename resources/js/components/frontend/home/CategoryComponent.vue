dae64569fb495be45949ea3f3b19c2fc5a5f9c1a
Swapnil-Parashar <swapnilparashar1001@gmail.com>
2024-05-01 20:11:55 +0530

    SP1903

--- resources/js/components/frontend/home/CategoryComponent.vue
+++ resources/js/components/frontend/home/CategoryComponent.vue
@@ -32,7 +32,7 @@
                             class="text-sm sm:text-xl font-medium capitalize text-center py-2 px-3 overflow-hidden block group-hover:text-primary flex-grow"
                         >
                             {{ category.name }}
-                            <template v-if="category.name.length < 15">
+                            <template v-if="{{category.name.length}} < 15">
                                 <span class="text-white"
                                     >...........................
                                 </span>
