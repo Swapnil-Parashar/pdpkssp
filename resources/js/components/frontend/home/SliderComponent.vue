e1a1c710a703e1dca4bd0c8e03e28b82f34325cd
Swapnil-Parashar <swapnilparashar1001@gmail.com>
2024-05-30 12:33:21 +0530

    SP1903

--- resources/js/components/frontend/home/SliderComponent.vue
+++ resources/js/components/frontend/home/SliderComponent.vue
@@ -17,7 +17,7 @@
                     <div v-if="slider.link">
                         <a :href="slider.link">
                             <img
-                                class="w-full min-md:h-80"
+                                class="w-full min-md:h-64"
                                 :src="slider.image"
                                 alt="banner0"
                                 loading="lazy"
@@ -26,7 +26,7 @@
                     </div>
                     <div v-else>
                         <img
-                            class="w-full min-md:h-80"
+                            class="w-full min-md:h-64"
                             :src="slider.image"
                             alt="banner"
                             loading="lazy"
