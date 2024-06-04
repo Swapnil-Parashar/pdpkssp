945d2651c4346f5f26f244d8969ab5e5cf6c8594
Swapnil-Parashar <swapnilparashar1001@gmail.com>
2024-06-04 16:47:31 +0530

    SP1903

--- resources/js/components/frontend/components/ProductListComponent.vue
+++ resources/js/components/frontend/components/ProductListComponent.vue
@@ -1,26 +1,60 @@
 <template>
-    <div v-if="products.length > 0" v-for="product in products"
-         class="sm:p-2 sm:shadow-card transition-all duration-300 sm:hover:shadow-hover group">
+    <div
+        v-if="products.length > 0"
+        v-for="product in products"
+        class="sm:p-2 sm:shadow-card transition-all duration-300 sm:hover:shadow-hover group"
+        style="
+            box-shadow: 0 1px 3px 1px rgba(0, 0, 0, 0.2);
+            border-radius: 0.25rem;
+        "
+    >
         <div class="relative overflow-hidden isolate">
-            <label class="capitalize text-xs font-semibold py-1 px-2 shadow-badge absolute top-3 left-3 z-10 bg-secondary text-white"
-                v-if="product.is_offer && product.flash_sale">{{ $t('label.flash_sale') }}</label>
+            <label
+                class="capitalize text-xs font-semibold py-1 px-2 shadow-badge absolute top-3 left-3 z-10 bg-secondary text-white"
+                v-if="product.is_offer && product.flash_sale"
+                >{{ $t("label.flash_sale") }}</label
+            >
 
-            <button type="button" @click.prevent="wishlist(product, product.wishlist = !product.wishlist)"
-                    :class="product.wishlist ? 'lab-fill-heart text-primary' : 'lab-line-heart'"
-                    class="w-7 h-7 leading-7 text-center text-base shadow-badge absolute top-3 right-3 z-10 bg-white">
-            </button>
+            <button
+                type="button"
+                @click.prevent="
+                    wishlist(product, (product.wishlist = !product.wishlist))
+                "
+                :class="
+                    product.wishlist
+                        ? 'lab-fill-heart text-primary'
+                        : 'lab-line-heart'
+                "
+                class="w-7 h-7 leading-7 text-center text-base shadow-badge absolute top-3 right-3 z-10 bg-white"
+            ></button>
 
-            <router-link class="overflow-hidden w-full"
-                         :to="{ name: 'frontend.product.details', params: { slug: product.slug }}">
-                <img :src="product.cover" alt="product"
-                     class="w-full transition-all duration-300 group-hover:scale-105 group-hover:rotate-3" loading="lazy">
+            <router-link
+                class="overflow-hidden w-full"
+                :to="{
+                    name: 'frontend.product.details',
+                    params: { slug: product.slug },
+                }"
+            >
+                <img
+                    :src="product.cover"
+                    alt="product"
+                    class="w-full transition-all duration-300 group-hover:scale-105 group-hover:rotate-3"
+                    loading="lazy"
+                />
             </router-link>
         </div>
 
         <div class="px-1 sm:px-0 pt-4 pb-2">
-            <h3 class="capitalize text-base font-semibold transition-all duration-300 hover:text-primary">
-                <router-link class="block overflow-hidden"
-                             :to="{ name: 'frontend.product.details', params: { slug: product.slug }}">
+            <h3
+                class="capitalize text-base font-semibold transition-all duration-300 hover:text-primary"
+            >
+                <router-link
+                    class="block overflow-hidden"
+                    :to="{
+                        name: 'frontend.product.details',
+                        params: { slug: product.slug },
+                    }"
+                >
                     {{ product.name }}
                 </router-link>
             </h3>
@@ -31,23 +65,53 @@
                         border-color="#FFBC1F"
                         :padding="2.5"
                         :border-width="2.5"
-                        :star-size="9" class="mt-[2px]" inactive-color="#FFFFFF"
+                        :star-size="9"
+                        class="mt-[2px]"
+                        inactive-color="#FFFFFF"
                         active-color="#FFBC1F"
-                        :round-start-rating="false" :show-rating="false" :read-only="true"
+                        :round-start-rating="false"
+                        :show-rating="false"
+                        :read-only="true"
                         :max-rating="5"
-                        :rating="(product.rating_star / product.rating_star_count)"/>
+                        :rating="
+                            product.rating_star / product.rating_star_count
+                        "
+                    />
                 </div>
-                <div v-if="product.rating_star_count > 0" class="flex items-center gap-1 mt-[5px]">
-                    <span class="text-xs font-medium whitespace-nowrap text-text">{{ (product.rating_star / product.rating_star_count).toFixed(1) }}</span>
-                    <span class="text-xs font-medium whitespace-nowrap text-text hover:text-primary">({{ product.rating_star_count }} {{ product.rating_star_count > 1 ? $t('label.reviews') : $t('label.review') }})</span>
+                <div
+                    v-if="product.rating_star_count > 0"
+                    class="flex items-center gap-1 mt-[5px]"
+                >
+                    <span
+                        class="text-xs font-medium whitespace-nowrap text-text"
+                        >{{
+                            (
+                                product.rating_star / product.rating_star_count
+                            ).toFixed(1)
+                        }}</span
+                    >
+                    <span
+                        class="text-xs font-medium whitespace-nowrap text-text hover:text-primary"
+                        >({{ product.rating_star_count }}
+                        {{
+                            product.rating_star_count > 1
+                                ? $t("label.reviews")
+                                : $t("label.review")
+                        }})</span
+                    >
                 </div>
             </div>
 
-            <div class="flex flex-wrap-reverse items-center gap-x-3 gap-y-1" v-if="product.is_offer">
+            <div
+                class="flex flex-wrap-reverse items-center gap-x-3 gap-y-1"
+                v-if="product.is_offer"
+            >
                 <h3 class="text-xl sm:text-[22px] font-bold">
                     <span>{{ product.discounted_price }}</span>
                 </h3>
-                <h4 class="text-sm sm:text-base font-semibold text-shopperz-red">
+                <h4
+                    class="text-sm sm:text-base font-semibold text-shopperz-red"
+                >
                     <del>{{ product.currency_price }}</del>
                 </h4>
             </div>
@@ -59,35 +123,36 @@
 </template>
 
 <script>
-
 import starRating from "vue-star-rating";
 import router from "../../../router";
 export default {
     name: "ProductListComponent",
     components: {
-        starRating
+        starRating,
     },
     props: {
-        "products": "object",
+        products: "object",
     },
     data() {
         return {
-            rating: []
-        }
+            rating: [],
+        };
     },
     methods: {
         wishlist: function (product, toggle) {
-            this.$store.dispatch("frontendWishlist/toggle", {
-                product_id: product.id,
-                toggle: toggle
-            }).then((res) => {
-            }).catch((err) => {
-                if (err.response.status === 401) {
-                    product.wishlist = false;
-                    router.push({name: "auth.login"});
-                }
-            });
+            this.$store
+                .dispatch("frontendWishlist/toggle", {
+                    product_id: product.id,
+                    toggle: toggle,
+                })
+                .then((res) => {})
+                .catch((err) => {
+                    if (err.response.status === 401) {
+                        product.wishlist = false;
+                        router.push({ name: "auth.login" });
+                    }
+                });
         },
-    }
-}
+    },
+};
 </script>
