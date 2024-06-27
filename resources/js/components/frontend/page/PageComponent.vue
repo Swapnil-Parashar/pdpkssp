f7e059d8dec0cf6906126a93dacae70bcbb0d7d2
My Name <getdrema@gmail.com>
2024-06-27 18:24:03 +0530

    SP1903

--- resources/js/components/frontend/page/PageComponent.vue
+++ resources/js/components/frontend/page/PageComponent.vue
@@ -1,20 +1,25 @@
 <template>
     <section class="mb-10 sm:mb-20">
-        <div class="container">
+        <div>
             <div v-if="page.title === 'About Us'">
                 <AboutUsComponent />
             </div>
-            <div v-else class="mb-6">
-                <h2 class="text-[26px] leading-10 font-semibold capitalize mb-2">
-                    {{ page.title }}
-                </h2>
-                <div v-if="page.image" class="w-full mb-6">
-                    <img :src="page.image" alt="image" loading="lazy">
+            <div v-else class="container">
+                <div class="mb-6">
+                    <h2
+                        class="text-[26px] leading-10 font-semibold capitalize mb-2"
+                    >
+                        {{ page.title }}
+                    </h2>
+                    <div v-if="page.image" class="w-full mb-6">
+                        <img :src="page.image" alt="image" loading="lazy" />
+                    </div>
+                    <div v-html="page.description"></div>
                 </div>
-                <div v-html="page.description">
-                </div>
+                <TemplateManagerComponent
+                    :menuTemplateId="page.menu_template_id"
+                />
             </div>
-            <TemplateManagerComponent :menuTemplateId="page.menu_template_id" />
         </div>
     </section>
 </template>
@@ -27,27 +32,33 @@ export default {
     name: "PageComponent",
     components: {
         TemplateManagerComponent,
-        AboutUsComponent
+        AboutUsComponent,
     },
     computed: {
         page() {
-            return this.$store.getters['frontendPage/show'];
-        }
+            return this.$store.getters["frontendPage/show"];
+        },
     },
     mounted() {
         this.pageSetup();
     },
     methods: {
         pageSetup() {
-            if (Object.keys(this.$route.params).length > 0 && typeof this.$route.params.slug === 'string') {
-                this.$store.dispatch('frontendPage/show', this.$route.params.slug).then().catch()
+            if (
+                Object.keys(this.$route.params).length > 0 &&
+                typeof this.$route.params.slug === "string"
+            ) {
+                this.$store
+                    .dispatch("frontendPage/show", this.$route.params.slug)
+                    .then()
+                    .catch();
             }
-        }
+        },
     },
     watch: {
         $route() {
             this.pageSetup();
-        }
-    }
-}
+        },
+    },
+};
 </script>
