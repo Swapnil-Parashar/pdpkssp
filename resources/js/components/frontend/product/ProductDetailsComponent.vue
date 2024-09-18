72d6991e3077125a1afd7491fd54f17ff10e27dd
Swapnil-Parashar <swapnilparashar1001@gmail.com>
2024-09-18 14:23:40 +0530

    SP1903

--- resources/js/components/frontend/product/ProductDetailsComponent.vue
+++ resources/js/components/frontend/product/ProductDetailsComponent.vue
@@ -23,7 +23,7 @@
                         >
                             <img
                                 class="w-full rounded-2xl"
-                                :src="image"
+                                :src="image.replace('/conversions/', '/').replace('-cover', '')"
                                 alt="gallery"
                                 loading="lazy"
                             />
@@ -47,7 +47,7 @@
                         >
                             <img
                                 class="w-full rounded-lg border-2 border-gray-200 transition-all duration-500"
-                                :src="image"
+                                :src="image.replace('/conversions/', '/').replace('-cover', '')"
                                 alt="gallery"
                                 loading="lazy"
                             />
@@ -252,17 +252,6 @@
                                 {{ $t("label.details") }}
                             </button>
 
-                            <!-- <button type="button"
-                                    @click.prevent="multiTargets($event, 'tab-btn', 'tab-div', 'tab_videos')"
-                                    class="tab-btn text-sm sm:text-base font-bold leading-5 capitalize py-2 sm:py-3.5 px-5 sm:px-8 rounded-full border border-[#D9DBE9]">
-                                {{ $t('label.videos') }}
-                            </button> -->
-
-                            <!-- <button type="button"
-                                    @click.prevent="multiTargets($event, 'tab-btn', 'tab-div', 'tab_reviews')"
-                                    class="tab-btn text-sm sm:text-base font-bold leading-5 capitalize py-2 sm:py-3.5 px-5 sm:px-8 rounded-full border border-[#D9DBE9]">
-                                {{ $t('label.reviews') }}
-                            </button> -->
                             <button
                                 type="button"
                                 @click.prevent="
@@ -293,76 +282,6 @@
                                 v-html="product.details"
                             ></div>
                         </div>
-
-                        <!-- <div id="tab_videos" class="tab-div p-4 sm:p-8 sm:pt-6 border-t border-[#D9DBE9]">
-                            <h3 class="capitalize text-2xl sm:text-3xl font-bold mb-4">
-                                {{ $t('label.product_videos') }}
-                            </h3>
-                            <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
-                                <iframe v-for="video in videos" :src="video.link"
-                                        class="w-full h-40 sm:h-64 rounded-2xl"></iframe>
-                            </div>
-                        </div>
-
-                        <div id="tab_reviews" class="tab-div p-4 sm:p-8 sm:pt-6 border-t border-[#D9DBE9]">
-                            <h3 class="capitalize text-2xl sm:text-3xl font-bold mb-4">
-                                {{ $t('label.product_reviews') }}
-                            </h3>
-                            <div class="flex flex-wrap items-center gap-2 border-b border-gray-100 mb-6 pb-6">
-                                <starRating
-                                    border-color="#FFBC1F"
-                                    :rounded-corners="true" :padding="2.5"
-                                    :border-width="2.5"
-                                    :star-size="11" class="-mt-0.5" inactive-color="#FFFFFF"
-                                    active-color="#FFBC1F"
-                                    :round-start-rating="false" :show-rating="false" :read-only="true"
-                                    :max-rating="5"
-                                    :rating="(product.rating_star / product.rating_star_count)"/>
-                                <div v-if="product.rating_star_count > 0" class="flex items-center gap-1">
-                                    <span class="text-base font-medium whitespace-nowrap text-text">
-                                        {{ (product.rating_star / product.rating_star_count).toFixed(1) }}
-                                    </span>
-                                    <span
-                                        class="text-base font-medium whitespace-nowrap text-text hover:text-primary cursor-pointer">
-                                        ({{
-                                            product.rating_star_count
-                                        }} {{
-                                            product.rating_star_count > 1 ? $t('label.reviews') : $t('label.review')
-                                        }})
-                                    </span>
-                                </div>
-                            </div>
-
-                            <div v-for="review in reviews" class="mb-8">
-                                <h4 class="text-lg font-semibold capitalize mb-2">{{ review.name }}</h4>
-                                <div class="flex flex-wrap items-center gap-2 mb-3">
-                                    <starRating
-                                        border-color="#FFBC1F"
-                                        inactive-color="#FFFFFF"
-                                        active-color="#FFBC1F"
-                                        :rounded-corners="true"
-                                        :padding="2.5" :border-width="2.5"
-                                        :star-size="11" class="-mt-0.5"
-                                        :round-start-rating="false"
-                                        :show-rating="false" :read-only="true"
-                                        :max-rating="5"
-                                        :rating="review.star"/>
-                                </div>
-                                <p class="mb-4">{{ review.review }}</p>
-
-
-                                <div class="flex flex-wrap gap-4" v-if="review.images.length > 0">
-                                    <img v-for="reviewImage in review.images" :src="reviewImage" alt="image" class="w-20 rounded-lg">
-                                </div>
-                            </div>
-
-                            <button v-if="product.rating_star_count > reviews.length" @click.prevent="readMore"
-                                    type="button"
-                                    class="flex items-center justify-center gap-3 w-fit mx-auto text-primary">
-                                <span class="text-lg font-medium capitalize">{{ $t('label.read_more') }}</span>
-                                <i class="lab-line-down-arrow text-sm -mt-1"></i>
-                            </button>
-                        </div> -->
 
                         <div
                             id="tab_shipping_and_return"
@@ -502,6 +421,7 @@ export default {
             return this.$store.getters["frontendProduct/show"];
         },
         images: function () {
+            console.log(this.$store.getters["frontendProduct/showImages"]);
             return this.$store.getters["frontendProduct/showImages"];
         },
         videos: function () {
